52a01db5b4b16f51a98483b6707219dafc818d3e
Justin Edwards <justin@igrowbeards.com>
2015-11-15 08:12:48 -0600

    use session for storage of game vars, create guess route

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+enable :sessions
+
 configure do
   sorted_dict = {}
   file = File.open('dict.txt','r')
@@ -11,22 +13,30 @@ configure do
     sorted_dict[word.length] << word
   end
   DICT = sorted_dict.sort.to_h
-
-  # game tracking variables
-  @@used_letters = []
-  @@difficulty = 0
 end
 
 get '/' do
+  session['used_letters'] = []
+  session['difficulty'] = 5
   slim :index, locals: { dict: DICT }
 end
 
 post '/start_game' do
-  @@difficulty = params['difficulty']
-  @@secret_word = DICT[params[:difficulty].to_i].sample
+  session['difficulty'] = params['difficulty']
+  session['secret_word'] = DICT[params[:difficulty].to_i].sample
   redirect '/play'
 end
 
 get '/play' do
-  slim :play, locals: { difficulty: @@difficulty, secret_word: @@secret_word }
+  slim :play, locals: { difficulty: session['difficulty'], secret_word: session['secret_word'], used_letters: session['used_letters'], message: session['message'] }
 end 
+
+post '/guess' do
+  session['used_letters'] << params['guess']
+  if session['secret_word'].include? params['guess']
+    session['message'] = 'Correct'
+  else
+    session['message'] = 'Incorrect'
+  end
+  redirect '/play'
+end
