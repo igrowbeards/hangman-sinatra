1d99db3f6778d50a312a316f7a46e081efbcac59
Justin Edwards <justin@igrowbeards.com>
2015-11-15 11:14:11 -0600

    fulfil win condition - light css for easier debugging

--- hangman.rb
+++ hangman.rb
@@ -16,14 +16,11 @@ configure do
 end
 
 get '/' do
-  session['used_letters'] = []
-  session['difficulty'] = 5
-  session['message'] = nil
-  session['guesses'] = 0
   slim :index, locals: { dict: DICT }
 end
 
 post '/start_game' do
+  cleanup_session
   session['difficulty'] = params['difficulty']
   session['secret_word'] = DICT[params[:difficulty].to_i].sample
   redirect '/play'
@@ -35,10 +32,31 @@ end
 
 post '/guess' do
   session['used_letters'] << params['guess'].strip
+  set_message
+  gameover?
+  redirect '/play'
+end
+
+def gameover?
+  # player has won if they have no more blanks in their secret word
+  uniqs = session['secret_word'].split('')
+  if ( uniqs - session['used_letters']) == []
+    session['message'] += ' - You Win!'
+  end
+  # player has lost if they go over the secret word length + 5
+end
+
+def cleanup_session
+  session['used_letters'] = []
+  session['difficulty'] = 5
+  session['message'] = nil
+  session['guesses'] = 0
+end
+
+def set_message
   if session['secret_word'].include? params['guess']
     session['message'] = 'Correct'
   else
     session['message'] = 'Incorrect'
   end
-  redirect '/play'
 end
